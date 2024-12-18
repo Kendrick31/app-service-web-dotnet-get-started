0480e34a3ede7f954838f5d1f022a9e25c48c87f
Brian-yws <v-chayuan@microsoft.com>
2024-12-18 13:45:26 +0800

    Update Global.asax.cs

--- aspnet-get-started/Global.asax.cs
+++ aspnet-get-started/Global.asax.cs
@@ -6,16 +6,34 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+public IActionResult Index()
+{
+    throw new Exception("This is a test exception");
+}
+
 namespace aspnet_get_started
 {
+    public IActionResult Index()
+    {
+        throw new Exception("This is a test exception");
+    }
     public class MvcApplication : System.Web.HttpApplication
     {
+        public IActionResult Index()
+        {
+            throw new Exception("This is a test exception");
+        }
         protected void Application_Start()
         {
+            public IActionResult Index()
+            {
+                throw new Exception("This is a test exception");
+            }
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Index()
         }
     }
 }
