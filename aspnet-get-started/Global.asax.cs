402b1e407534a1a61844fce286d31d818a039622
Brian-yws <v-chayuan@microsoft.com>
2024-12-19 16:02:38 +0800

    Update Global.asax.cs

--- aspnet-get-started/Global.asax.cs
+++ aspnet-get-started/Global.asax.cs
@@ -10,27 +10,6 @@ using System.Web.Routing;
 
 namespace aspnet_get_started
 {
-
-    public class Global : HttpApplication
-    {
-       public static log4net.ILog logger;
-    
-       void Application_Start(object sender, EventArgs e)
-       {
-           // Code that runs on application startup
-           RouteConfig.RegisterRoutes(RouteTable.Routes);
-           BundleConfig.RegisterBundles(BundleTable.Bundles);
-    
-           // Setup Application Insights
-           TelemetryConfiguration config = TelemetryConfiguration.Active;
-           TelemetryConfiguration.Active.ConnectionString = "InstrumentationKey=8ad68a6f-fb39-430f-9eda-ec54a5567706;IngestionEndpoint=https://southeastasia-1.in.applicationinsights.azure.com/;LiveEndpoint=https://southeastasia.livediagnostics.monitor.azure.com/;ApplicationId=52240843-2df1-4a68-a1a8-fe706bd245a3";
-    
-           // Setup log4net logger object
-           log4net.Config.XmlConfigurator.Configure();
-           logger = log4net.LogManager.GetLogger(this.GetType());
-       }
-    }
-
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
